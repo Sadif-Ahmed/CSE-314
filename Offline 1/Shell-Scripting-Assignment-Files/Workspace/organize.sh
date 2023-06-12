9ad6db2635c989d447c4b1b4d82062efed09e364
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-06-13 00:43:51 +0600

    Work in progress

--- Offline 1/Shell-Scripting-Assignment-Files/Workspace/organize.sh
+++ Offline 1/Shell-Scripting-Assignment-Files/Workspace/organize.sh
@@ -4,6 +4,13 @@ mkdir -p $2/C
 mkdir -p $2/Python
 mkdir -p $2/Java
 mkdir -p unzipped
+
+for file in "$1"/*
+do
+#echo "${file: -11}"
+mkdir -p unzipped/${file: -11}
+unzip "$file" -d unzipped/${file: -11}
+done
 count_test()
 {
 	count=0
@@ -13,21 +20,6 @@ count_test()
 	done
 	echo "Found $count test files"
 }
-if [ $# -gt 4 ]
-then
-if [ $5 = "-v" ]
-then
-count_test $3
-fi
-fi
-
-for file in "$1"/*
-do
-#echo "${file: -11}"
-mkdir -p unzipped/${file: -11}
-unzip "$file" -d unzipped/${file: -11}
-done
-
 visit_and_organise()
 {
 	if [ -d "$1" ]
@@ -35,31 +27,50 @@ visit_and_organise()
 	
 		for i in "$1"/*
 		do
-			visit_and_organise "$i" $2 
+			visit_and_organise "$i" $2 $3
 		done
 	
 	elif [ -f "$1" ]
 	then
 	#echo "$i"
 	#echo "${i:9:7}"
+	if [ $3 -eq 1 ]
+	then
+	echo "Organising files for ${i:9:7} "
+	fi
 	if [ "${i: -2}" =  ".c" ]
 	then
-	echo "c file"
+	#echo "c file"
 	mkdir -p $2/C/${i:9:7}
 	cp -p "$i" "$2/C/${i:9:7}/main.c"	
 	elif [ "${i: -3}" =  ".py" ]
 	then
-	echo "python file"
+	#echo "python file"
 	mkdir -p $2/Python/${i:9:7}
 	cp -p "$i" "$2/Python/${i:9:7}/main.py"
 	elif [ "${i: -5}" =  ".java" ]
 	then
-	echo "java file"
+	#echo "java file"
 	mkdir -p $2/Java/${i:9:7}
 	cp -p "$i" "$2/Java/${i:9:7}/Main.java"
 	fi	
 	fi
 }
+if [ $# -gt 4 ]
+then
+if [ $5 = "-v" ]
+then
+count_test $3
+fi
+fi
+if [ $# -gt 4 ]
+then
+if [ $5 = "-v" ]
+then
+visit_and_organise unzipped $2 1
+else
+visit_and_organise unzipped $2 0
+fi
+fi
 
-visit_and_organise unzipped $2 
 rm -r unzipped
