a6a936a977f470cca40194c374c70ea58ef43a34
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-06-13 00:34:00 +0600

    Organising on work

--- Offline 1/Shell-Scripting-Assignment-Files/Workspace/organize.sh
+++ Offline 1/Shell-Scripting-Assignment-Files/Workspace/organize.sh
@@ -20,11 +20,12 @@ then
 count_test $3
 fi
 fi
+
 for file in "$1"/*
 do
 #echo "${file: -11}"
 mkdir -p unzipped/${file: -11}
-unzip "$file" -dq unzipped/${file: -11}
+unzip "$file" -d unzipped/${file: -11}
 done
 
 visit_and_organise()
@@ -34,7 +35,7 @@ visit_and_organise()
 	
 		for i in "$1"/*
 		do
-			visit_and_organise "$i" $2 -v
+			visit_and_organise "$i" $2 
 		done
 	
 	elif [ -f "$1" ]
@@ -43,22 +44,22 @@ visit_and_organise()
 	#echo "${i:9:7}"
 	if [ "${i: -2}" =  ".c" ]
 	then
-	#echo "c file"
+	echo "c file"
 	mkdir -p $2/C/${i:9:7}
 	cp -p "$i" "$2/C/${i:9:7}/main.c"	
 	elif [ "${i: -3}" =  ".py" ]
 	then
-	#echo "python file"
+	echo "python file"
 	mkdir -p $2/Python/${i:9:7}
 	cp -p "$i" "$2/Python/${i:9:7}/main.py"
 	elif [ "${i: -5}" =  ".java" ]
 	then
-	#echo "java file"
+	echo "java file"
 	mkdir -p $2/Java/${i:9:7}
 	cp -p "$i" "$2/Java/${i:9:7}/Main.java"
 	fi	
 	fi
 }
 
-visit_and_organise unzipped $2 -v
+visit_and_organise unzipped $2 
 rm -r unzipped
