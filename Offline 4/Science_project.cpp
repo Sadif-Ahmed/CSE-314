569d7ab0ea8468a1099201d10320d9703a7e08a8
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-08-04 20:48:24 +0600

    Added Comments

--- Offline 4/Science_project.cpp
+++ Offline 4/Science_project.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int n,m,w,x,y;
 
 default_random_engine generator;
-poisson_distribution<int> distribution(4.1);
+poisson_distribution<int> distribution(9.9);
 int rand_mod=5;
 int rand_number;
 
@@ -68,9 +68,13 @@ class student
     {
         printf("The student id: %d\n",get_id());
     }
-    void printing_start()
+    void printing_arrival()
     {
         printf("Student %d has arrived at the print station at time %d\n",std_id,get_curr_time());
+    }
+    void printing_start()
+    {
+        printf("Student %d has started printing at time %d\n",std_id,get_curr_time());
     }
     void printing_end()
     {
@@ -142,6 +146,10 @@ class group
     {
         printf("Group %d has finished printing at time %d\n",grp_id,get_curr_time());
     }
+    void binding_arrival()
+    {
+        printf("Group %d has arrived at binding station at time %d\n",grp_id,get_curr_time());
+    }
     void binding_start()
     {
         printf("Group %d has started binding at time %d\n",grp_id,get_curr_time());
@@ -149,6 +157,10 @@ class group
     void binding_end()
     {
         printf("Group %d has finished binding at time %d\n",grp_id,get_curr_time());
+    }
+    void submit_arrival()
+    {
+        printf("Group %d has arrived at entry-book station at time %d\n",grp_id,get_curr_time());
     }
     void submit_start()
     {
@@ -221,12 +233,13 @@ class staff
 
         rand_number = distribution(generator)%rand_mod + 1;
         sleep(rand_number);
-
+        //Printing station arrival
+        std->printing_arrival();
         int printer_id = std->get_id()%number_of_printers;
         pthread_mutex_lock(&printer_locks[printer_id]);
-
         if(printer_status[printer_id])
         {
+            //Printer Busy so block
             pthread_mutex_unlock(&printer_locks[printer_id]);
             pthread_mutex_lock(&std->waiting_lock);
             std->waiting=true;
@@ -235,38 +248,46 @@ class staff
         }
         else
         {
+            //Printer Free so mark it busy and proceed
             printer_status[printer_id]=true;
             pthread_mutex_unlock(&printer_locks[printer_id]);
         }
+        //Printing
         std->printing_start();
         sleep(w);
         std->printing_end();
         pthread_mutex_lock(&printer_locks[printer_id]);
         printer_status[printer_id]=false;
         pthread_mutex_unlock(&printer_locks[printer_id]);
-
+        //Printing Done.Notifying people waiting on the printer
         std->get_group()->notify(std);
+        //Informing leader of work done
         sem_post(std->get_group()->get_print_done_sem()); 
         if(!std->is_leader)
         {
             pthread_exit(NULL);
         }
+        //Group leader checks if all members have informed or not.If not blocks and waits.
         for(int i=0;i<std->get_group()->students.size();i++)
         {
           sem_wait(std->get_group()->get_print_done_sem());   
         }
         std->get_group()->printing_end();
-
+        //Group Print Done.Go to binder. 
+        std->get_group()->binding_arrival();
         sem_wait(&binder_sem);
+        //Started Binding
         std->get_group()->binding_start();
         sleep(x);
         std->get_group()->binding_end();
         sem_post(&binder_sem);
-
-        std->get_group()->submit_start();
+        //Ended Binding.Start Submitting.
+        std->get_group()->submit_arrival();
         staff::accept_submission();
+        std->get_group()->submit_start();
         sleep(y);
         std->get_group()->submit_end();
+        //All done exit
         pthread_exit(NULL);
     }
 void * staff_work(void *st)
@@ -277,6 +298,7 @@ void * staff_work(void *st)
     //printf("I am inside staff %d\n",stf->staff_id);
     while(true)
     {
+        //Constant Reader Update.Readers get priority.
         pthread_mutex_lock(&reader_lock);
         reader_count++;
         if(reader_count==1)
@@ -381,17 +403,26 @@ int main()
             printf("Error while creating thread\n");
             exit(-1);
         }
-    }
-    for(int i=0;i<2;i++)
-    {
-        
-        stat=pthread_create(&staff_threads[i],NULL,staff_work,(void *)staffs[i]);
+        if( i == w/2)
+        {
+        stat=pthread_create(&staff_threads[0],NULL,staff_work,(void *)staffs[0]);
         if(stat)
         {
             printf("Error while creating thread\n");
             exit(-1);
         }
+        }
+        else if(i == w/7)
+        {
+            stat=pthread_create(&staff_threads[1],NULL,staff_work,(void *)staffs[1]);
+        if(stat)
+        {
+            printf("Error while creating thread\n");
+            exit(-1);
+        }
+        }
     }
+    
     pthread_exit(NULL);
     return 0;
  }
